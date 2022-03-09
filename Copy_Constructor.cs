c019c87ed10ff71a294ad28cac0abf22100377aa
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2022-03-09 09:38:33 +0530

    Copy Constructor

---
+++ Copy_Constructor.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Copy_Constructor
+{
+    class Program
+    {
+       static void Costructor(int a,int b)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
