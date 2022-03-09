b6c00e3eecc1983bd9fb690189550de5695f6a3c
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2022-03-09 09:42:27 +0530

    While Loop

---
+++ WhileLoop.cs
@@ -0,0 +1,18 @@
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (count<5)
+            {
+                Console.WriteLine("cont is"+count);
+                count++;
+            }
+        }
+    }
+}
