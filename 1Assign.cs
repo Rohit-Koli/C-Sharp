b5f9059bf761fcd6a7e6d90b042d4b0d8537a328
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2022-04-30 12:32:53 +0530

    Create 1Assign.cs

---
+++ 1Assign.cs
@@ -0,0 +1,12 @@
+using System;
+namespace 1Assign
+{
+  class Program
+  {
+    int n=Convert.ToInt32(Console.ReadLine());
+    for(int i=1;i<=n;i++)
+    {
+      Console.WriteLine("Each one teach one");
+    }
+  }
+}
