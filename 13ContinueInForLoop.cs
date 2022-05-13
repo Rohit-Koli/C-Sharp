62a1e4178beab99b4b009f1fbfe542d0a2fff7a8
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2022-05-13 17:55:59 +0530

    Update 13ContinueInForLoop.cs

--- 13ContinueInForLoop.cs
+++ 13ContinueInForLoop.cs
@@ -1,14 +1,15 @@
 Using System;
 Namespace CSharp_Shell
-{    Pu
-blic class Program 
+{
+    Public class Program 
     {
         Public static void Main()
-        {	Int I,n;
-			Console.WriteLine(“Enter a number:-“);
-		    N=Convert.ToInt32(Console.ReadLine());
-		    Console.WriteLine(“seris of 1 to 10 in for loop:-“);
-		    For(i=1;i<=n;i++)
-		    {
-		    	Console.WriteLine(i); 
-		    }		    Console.ReadLine();        }     }   }
+        {
+	For(int x = 1; x <= 12; x++) 
+    { 
+         If (x == 3) 
+           { 
+               Continue; 
+            } 
+            Console.WriteLine(x);
+        }     }     }   }
